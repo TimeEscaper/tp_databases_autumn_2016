e6ecd3fa7b873085fd74d5e5b981fc6f44539a1c
TimeEscaper <rentgeny05@gmail.com>
2016-11-22 22:10:28 +0300

    PostDataSet created.

---
+++ src/main/java/ru/forum/model/PostDataSet.java
@@ -0,0 +1,82 @@
+package ru.forum.model;
+
+@SuppressWarnings("unused")
+public class PostDataSet {
+
+    private long id;
+    private long thread;
+    private String forum;
+    private String user;
+    private String message;
+    private String date;
+    private long parent;
+    private boolean isApproved;
+    private boolean isHighlighted;
+    private boolean isEdited;
+    private boolean isSpam;
+    private boolean isDeleted;
+
+    public PostDataSet(long id, long thread, String forum, String user, String message, String date, long parent,
+                       boolean isApproved, boolean isHighlighted, boolean isEdited, boolean isSpam, boolean isDeleted) {
+        this.id = id;
+        this.thread = thread;
+        this.forum = forum;
+        this.user = user;
+        this.message = message;
+        this.date = date;
+        this.parent = parent;
+        this.isApproved = isApproved;
+        this.isHighlighted = isHighlighted;
+        this.isEdited = isEdited;
+        this.isSpam = isSpam;
+        this.isDeleted = isDeleted;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public long getThread() {
+        return thread;
+    }
+
+    public String getForum() {
+        return forum;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public long getParent() {
+        return parent;
+    }
+
+    public boolean isApproved() {
+        return isApproved;
+    }
+
+    public boolean isHighlighted() {
+        return isHighlighted;
+    }
+
+    public boolean isEdited() {
+        return isEdited;
+    }
+
+    public boolean isSpam() {
+        return isSpam;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+}
