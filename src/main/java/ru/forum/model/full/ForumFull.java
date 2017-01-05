69daed6d2c8f6be88687a429a48580e0011a7a7c
TimeEscaper <rentgeny05@gmail.com>
2017-01-05 23:47:31 +0300

    ForumFull.

---
+++ src/main/java/ru/forum/model/full/ForumFull.java
@@ -0,0 +1,36 @@
+package ru.forum.model.full;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@SuppressWarnings("unused")
+public class ForumFull {
+    private long id;
+    private String name;
+    private String shortName;
+    private Object user;
+
+
+    public ForumFull(long id, String name, String shortName, Object user) {
+        this.id = id;
+        this.name = name;
+        this.shortName = shortName;
+        this.user = user;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("short_name")
+    public String getShortName() {
+        return shortName;
+    }
+
+    public Object getUser() {
+        return user;
+    }
+}
